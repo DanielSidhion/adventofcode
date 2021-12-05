1b71f1d949c39a3e734aab2a96375b4ac557a38e
DS <me@sidhion.com>
2021-12-05 06:18:16 -0800

    Day 5 part 2 solution in rust.

--- rust/src/day5.rs
+++ rust/src/day5.rs
@@ -1,12 +1,14 @@
 const FIELD_SIZE: usize = 1000;
 
 pub struct Submarine {
+    vents_field_no_diagonal: Vec<i32>,
     vents_field: Vec<i32>,
 }
 
 impl Submarine {
     pub fn new() -> Self {
         Self {
+            vents_field_no_diagonal: vec![0; FIELD_SIZE * FIELD_SIZE],
             vents_field: vec![0; FIELD_SIZE * FIELD_SIZE],
         }
     }
@@ -19,40 +21,52 @@ impl Submarine {
     }
 
     fn mark_vent(&mut self, x0: i32, y0: i32, x1: i32, y1: i32) {
-        // In case x0 != x1 and y0 != y1 we'll ignore, since part 1 doesn't care about those.
-
         if x0 == x1 {
-            let range = if y0 > y1 {
-                y1..(y0 + 1)
-            } else {
-                y0..(y1 + 1)
-            };
+            let range = build_inclusive_range(y0, y1);
 
             for y in range {
-                self.mark_location(x0, y);
+                self.mark_location(x0, y, false);
             }
         } else if y0 == y1 {
-            let range = if x0 > x1 {
-                x1..(x0 + 1)
-            } else {
-                x0..(x1 + 1)
-            };
+            let range = build_inclusive_range(x0, x1);
 
             for x in range {
-                self.mark_location(x, y0);
+                self.mark_location(x, y0, false);
+            }
+        } else {
+            let xrange = build_inclusive_range(x0, x1);
+            let yrange = build_inclusive_range(y0, y1);
+
+            for (x, y) in xrange.into_iter().zip(yrange) {
+                self.mark_location(x, y, true);
             }
         }
     }
 
-    fn mark_location(&mut self, x: i32, y: i32) {
+    fn mark_location(&mut self, x: i32, y: i32, is_diagonal: bool) {
         let index: usize = (x + y * FIELD_SIZE as i32).try_into().unwrap();
 
         self.vents_field[index] += 1;
+
+        if !is_diagonal {
+            self.vents_field_no_diagonal[index] += 1;
+        }
     }
 
     pub fn output(&self) {
+        let dangerous_areas_no_diagonal = self.vents_field_no_diagonal.iter().filter(|&&x| x > 1).count();
         let dangerous_areas = self.vents_field.iter().filter(|&&x| x > 1).count();
 
-        println!("Part 1: {}", dangerous_areas);
+        println!("Part 1: {}", dangerous_areas_no_diagonal);
+        println!("Part 2: {}", dangerous_areas);
+    }
+}
+
+// Output is a Vec<i32> to make both branches of the function body return the same type.
+fn build_inclusive_range(a: i32, b: i32) -> Vec<i32> {
+    if a > b {
+        (b..(a + 1)).rev().collect()
+    } else {
+        (a..(b + 1)).collect()
     }
 }
