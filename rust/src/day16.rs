271e834876e412b9be0f50f3d9ee993bab3f76b1
DS <me@sidhion.com>
2021-12-16 11:49:14 -0800

    Simplify some Day 16 rust code to adhere to the specification. Add comments for posteriority.

--- rust/src/day16.rs
+++ rust/src/day16.rs
@@ -13,6 +13,7 @@ struct Packet {
 
 impl Packet {
     pub fn evaluate(&self) -> usize {
+        // We treat the literal packet here because every other case is an operator packet, so we'll be able to make some simplifications because we know that.
         if self.type_id == 4 {
             if let PacketData::Literal { value } = self.data {
                 return value as usize;
@@ -21,10 +22,11 @@ impl Packet {
             }
         }
 
-        let subpackets = if let PacketData::Operator { subpackets } = &self.data {
+        // Since we know now we're dealing with an operator packet, we can unpack the packet data into a vec to make the code easier to read.
+        let subpackets: Vec<_> = if let PacketData::Operator { subpackets } = &self.data {
             subpackets.iter().collect()
         } else {
-            Vec::new()
+            panic!("We were assuming we'd have an operator packet in the code, but got something else!");
         };
 
         match self.type_id {
@@ -64,6 +66,7 @@ impl FromStr for Transmission {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let bytevec = s.chars()
             .map(|c| c.to_digit(16).unwrap())
+            // Will convert each pair of parsed hex digits into an `u8`. Due to how `scan` works, we need to wrap stuff in an `Option`, but we'll clean that up later.
             .scan((0u8, false), |state, d| {
                 if !state.1 {
                     *state = (d as u8, true);
@@ -74,6 +77,7 @@ impl FromStr for Transmission {
                     Some(Some(final_num))
                 }
             })
+            // Clean up the results of `scan`.
             .filter_map(std::convert::identity)
             .collect();
 
@@ -85,6 +89,7 @@ impl FromStr for Transmission {
     }
 }
 
+// Helper struct to allow iteration through the packets without consuming the `Transmission` struct itself.
 struct TransmissionIter<'a> {
     transmission: &'a mut Transmission,
     top_level_packet: bool,
@@ -94,11 +99,12 @@ impl<'a> Iterator for TransmissionIter<'a> {
     type Item = Packet;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let is_top_level = self.top_level_packet;
-
         if self.transmission.reached_end() {
             return None;
         }
+
+        // Only used so we can skip over the padding (if any) if we're the top level packet.
+        let is_top_level = self.top_level_packet;
 
         let version: u8 = self.transmission.next_bits(3);
         let type_id: u8 = self.transmission.next_bits(3);
@@ -107,19 +113,19 @@ impl<'a> Iterator for TransmissionIter<'a> {
             let mut literal: u64 = 0;
 
             loop {
-                let next_bit: u8 = self.transmission.next_bits(1);
+                let is_last_chunk: u8 = self.transmission.next_bits(1);
                 literal = (literal << 4) | self.transmission.next_bits::<u64>(4);
 
-                if next_bit == 0 {
+                if is_last_chunk == 0 {
                     break;
                 }
             }
 
-            Some(Packet {
+            Packet {
                 version,
                 type_id,
                 data: PacketData::Literal { value: literal },
-            })
+            }
         } else {
             let length_type_id: u8 = self.transmission.next_bits(1);
 
@@ -130,8 +136,10 @@ impl<'a> Iterator for TransmissionIter<'a> {
             };
 
             let mut subpackets = Vec::new();
+            // Since we'll read subpackets, must set that we're not in the top level anymore.
             self.top_level_packet = false;
 
+            // Doesn't matter if we're reading until x bits or reading x packets, we'll count down to 0.
             while length_value > 0 {
                 let starting_position = self.transmission.current_bit_pos;
                 let next_packet = self.next().unwrap();
@@ -146,19 +154,19 @@ impl<'a> Iterator for TransmissionIter<'a> {
                 subpackets.push(next_packet);
             }
 
-            Some(Packet {
+            Packet {
                 version,
                 type_id,
                 data: PacketData::Operator { subpackets },
-            })
+            }
         };
 
+        // Skip over padding. If we wanted to, we could reset the top level flag here to support reading multiple top-level packets, but the specification states that there will only be one top-level packet, so we stick to that.
         if is_top_level {
-            self.transmission.realign_checked();
-            self.top_level_packet = true;
+            self.transmission.realign();
         }
 
-        return_packet
+        Some(return_packet)
     }
 }
 
@@ -182,6 +190,7 @@ impl Transmission {
         self.current_bit_pos == self.raw_data.len() * 8
     }
 
+    // Takes the next amount of bits requested and returns as the type requested.
     fn next_bits<T: Default + TryFrom<usize>>(&mut self, num_bits: usize) -> T {
         let mut result: usize = 0;
 
@@ -228,7 +237,7 @@ impl Transmission {
     }
 
     // Skips over any padding to realign the current bit position to a multiple of 8. Ensures that the padding that was skipped was all 0.
-    fn realign_checked(&mut self) {
+    fn _realign_checked(&mut self) {
         if self.current_bit_pos % 8 == 0 {
             return;
         }
@@ -243,8 +252,12 @@ impl Transmission {
     }
 
     // Unchecked version of `realign_checked`.
-    fn _realign(&mut self) {
-        self.current_bit_pos = self.current_bit_pos + (self.current_bit_pos % 8);
+    fn realign(&mut self) {
+        if self.current_bit_pos % 8 == 0 {
+            return;
+        }
+
+        self.current_bit_pos = self.current_bit_pos + 8 - (self.current_bit_pos % 8);
     }
 }
 
@@ -264,13 +277,11 @@ impl Submarine {
     }
 
     pub fn output(&mut self) {
-        let mut total_version_sum: usize = 0;
-        let mut outermost_packet_value = 0;
+        // Specification states that there will only be one outermost packet. If there were more, we could just keep going through the iterator (would also need to change a flag in the iterator implementation).
+        let outermost_packet = self.transmission.iter_mut().next().unwrap();
 
-        for packet in self.transmission.iter_mut() {
-            total_version_sum += version_sum(&packet);
-            outermost_packet_value = packet.evaluate();
-        }
+        let total_version_sum = version_sum(&outermost_packet);
+        let outermost_packet_value = outermost_packet.evaluate();
 
         println!("Part 1: {}", total_version_sum);
         println!("Part 2: {}", outermost_packet_value);
