d3cfad0039d6445162c828670a3fe9a8674abadb
DS <me@sidhion.com>
2021-12-20 23:08:35 -0800

    Deduplicate code for Day 21 rust solution.

--- rust/src/day21.rs
+++ rust/src/day21.rs
@@ -1,68 +1,61 @@
 use std::collections::HashMap;
 
-#[derive(Eq, Hash, PartialEq)]
+#[derive(Clone, Eq, Hash, PartialEq)]
 struct Universe {
-    positions: (u8, u8),
-    scores: (u8, u8),
+    positions: [u8; 2],
+    scores: [u16; 2],
+    next_player: usize,
 }
 
 impl Universe {
-    pub fn new(positions: (u8, u8), scores: (u8, u8)) -> Self {
-        Universe { positions, scores }
+    pub fn new(positions: [u8; 2], scores: [u16; 2]) -> Self {
+        Universe { positions, scores, next_player: 0 }
     }
 }
 
 pub struct Submarine {
-    positions: (u8, u8),
-    scores: (u32, u32),
-    die_rolls: u32,
-    universe_scores: (u64, u64),
+    starting_positions: [u8; 2],
+    d100_rolls: u32,
+    universe_scores: [u64; 2],
 }
 
 impl Submarine {
     pub fn new() -> Self {
         Self {
-            positions: (0, 0),
-            scores: (0, 0),
-            die_rolls: 0,
-            universe_scores: (0, 0),
+            starting_positions: [0, 0],
+            d100_rolls: 0,
+            universe_scores: [0, 0],
         }
     }
 
     pub fn on_input(&mut self, result: &str) {
-        if self.positions.0 == 0 {
-            self.positions.0 = result.strip_prefix("Player 1 starting position: ").unwrap().parse::<u8>().unwrap() - 1;
+        if self.starting_positions[0] == 0 {
+            self.starting_positions[0] = result.strip_prefix("Player 1 starting position: ").unwrap().parse::<u8>().unwrap() - 1;
         } else {
-            self.positions.1 = result.strip_prefix("Player 2 starting position: ").unwrap().parse::<u8>().unwrap() - 1;
+            self.starting_positions[1] = result.strip_prefix("Player 2 starting position: ").unwrap().parse::<u8>().unwrap() - 1;
         }
     }
 
     // Returns the number of positions to move forward after all the complete rotations around the board.
     fn roll_d100(&mut self) -> u8 {
-        let res = 1 + (self.die_rolls % 100);
-        self.die_rolls += 1;
+        let res = 1 + (self.d100_rolls % 100);
+        self.d100_rolls += 1;
         (res % 10) as u8
     }
 
-    fn run_practice_game(&mut self) {
-        let mut game_positions = self.positions;
+    fn run_practice_game(&mut self) -> Universe {
+        let mut universe = Universe::new(self.starting_positions.clone(), [0, 0]);
 
         loop {
-            let p1_dice = self.roll_d100() + self.roll_d100() + self.roll_d100();
-            game_positions.0 = (game_positions.0 + p1_dice) % 10;
-            self.scores.0 += (1 + game_positions.0) as u32;
+            universe.positions[universe.next_player] += self.roll_d100() + self.roll_d100() + self.roll_d100();
+            universe.positions[universe.next_player] %= 10;
+            universe.scores[universe.next_player] += (1 + universe.positions[universe.next_player]) as u16;
 
-            if self.scores.0 >= 1000 {
-                return;
+            if universe.scores[universe.next_player] >= 1000 {
+                return universe;
             }
 
-            let p2_dice = self.roll_d100() + self.roll_d100() + self.roll_d100();
-            game_positions.1 = (game_positions.1 + p2_dice) % 10;
-            self.scores.1 += (1 + game_positions.1) as u32;
-
-            if self.scores.1 >= 1000 {
-                return;
-            }
+            universe.next_player = (universe.next_player + 1) % 2;
         }
     }
 
@@ -70,51 +63,26 @@ impl Submarine {
         let dirac_possibilities = generate_dirac_universes(3);
 
         let mut universes: HashMap<Universe, u64> = HashMap::new();
-        let starting_universe = Universe::new(self.positions, (0, 0));
+        let starting_universe = Universe::new(self.starting_positions, [0, 0]);
         universes.insert(starting_universe, 1);
 
         loop {
-            if universes.len() > 500_000 {
-                println!("WARNING");
-                return;
-            }
-
             let mut new_universes: HashMap<Universe, u64> = HashMap::new();
 
             for (dice_sum, possibilities) in dirac_possibilities.iter().enumerate() {
-                for (universe, amount) in universes.iter() {
-                    let p1_location = (universe.positions.0 + (dice_sum as u8) + 3) % 10;
-                    let p1_score = universe.scores.0 + p1_location + 1;
+                for (u, amount) in universes.iter() {
+                    let new_position = (u.positions[u.next_player] + (dice_sum as u8) + 3) % 10;
+                    let new_score = u.scores[u.next_player] + (new_position as u16) + 1;
 
-                    if p1_score >= 21 {
-                        self.universe_scores.0 += amount * possibilities;
+                    if new_score >= 21 {
+                        self.universe_scores[u.next_player] += amount * possibilities;
                         continue;
                     }
 
-                    let new_universe = Universe::new((p1_location, universe.positions.1), (p1_score, universe.scores.1));
-                    *new_universes.entry(new_universe).or_default() += amount * possibilities;
-                }
-            }
-
-            if new_universes.len() == 0 {
-                return;
-            }
-
-            universes = new_universes;
-
-            new_universes = HashMap::new();
-
-            for (dice_sum, possibilities) in dirac_possibilities.iter().enumerate() {
-                for (universe, amount) in universes.iter() {
-                    let p2_location = (universe.positions.1 + (dice_sum as u8) + 3) % 10;
-                    let p2_score = universe.scores.1 + p2_location + 1;
-
-                    if p2_score >= 21 {
-                        self.universe_scores.1 += amount * possibilities;
-                        continue;
-                    }
-
-                    let new_universe = Universe::new((universe.positions.0, p2_location), (universe.scores.0, p2_score));
+                    let mut new_universe = u.clone();
+                    new_universe.positions[u.next_player] = new_position;
+                    new_universe.scores[u.next_player] = new_score;
+                    new_universe.next_player = (new_universe.next_player + 1) % 2;
                     *new_universes.entry(new_universe).or_default() += amount * possibilities;
                 }
             }
@@ -128,16 +96,14 @@ impl Submarine {
     }
 
     pub fn output(&mut self) {
-        self.run_practice_game();
+        let practice_universe = self.run_practice_game();
 
-        let loser_score = self.scores.0.min(self.scores.1);
-
-        println!("Part 1: {}", loser_score * self.die_rolls);
+        let loser_score = *practice_universe.scores.iter().min().unwrap() as u32;
+        println!("Part 1: {}", loser_score * self.d100_rolls);
 
         self.run_dirac_game();
 
-        let winner_universes = self.universe_scores.0.max(self.universe_scores.1);
-
+        let winner_universes = self.universe_scores.iter().max().unwrap();
         println!("Part 2: {}", winner_universes);
     }
 }
